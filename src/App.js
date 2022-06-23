1ca49ff356d9bc6972d546a57138586904990062
Muhammad Enamul Haque <enahaque1164@gmail.com>
2022-06-23 13:15:28 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <h1>My-Rest-Assignment9</h1>
       <h2>I am trying to submit the rest of assignment in deadline</h2>
+      <Header></Header>
     </div>
   );
 }
